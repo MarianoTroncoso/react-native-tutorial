e99d46fd46da200f9a7344c33c08a6ba33b30f0f
Mariano Troncoso <petotronco@gmail.com>
2021-08-21 10:12:56 -0300

    add remote repo

--- App.js
+++ App.js
@@ -1,21 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
-export default function App() {
+const App = () => {
+  const [seletedImage, setSeletedImage] = useState(null);
+
+  const openImagePicker = async () => {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("permission to access camera is required");
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSeletedImage({ localUri: pickerResult.uri });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Pick an image</Text>
+      <Image
+        source={{
+          uri:
+            seletedImage !== null
+              ? seletedImage.localUri
+              : "https://picsum.photos/200/200",
+        }}
+        style={styles.image}
+      ></Image>
+      <TouchableOpacity onPress={openImagePicker} style={styles.button}>
+        <Text style={styles.buttonText}>Press me</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "lightpink",
+    color: "darkslategrey",
+  },
+  title: {
+    fontSize: 30,
+    color: "rebeccapurple",
+    fontWeight: "500",
+  },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    resizeMode: "contain",
+  },
+  button: {
+    backgroundColor: "lightcoral",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+    textTransform: "uppercase",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 20,
+    textTransform: "uppercase",
   },
 });
+
+export default App;
